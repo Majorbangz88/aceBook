dbcadb88ba76d4ca78157bc3d9d70fad72d5fa8f
Majorbangz88 <joelchimaobichukwu@gmail.com>
2023-12-27 10:58:18 +0100

    Ace Book app

---
+++ src/main/java/com/majorbangz88/aceBookApp/data/models/AccountDetails.java
@@ -0,0 +1,12 @@
+package com.majorbangz88.aceBookApp.data.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountDetails {
+
+    private String email;
+    private String password;
+}
---
+++ src/test/java/com/majorbangz88/aceBookApp/AceBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majorbangz88.aceBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
